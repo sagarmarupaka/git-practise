2238c0c7f06884a463918dbbc26c1dc156ae848f
sagarmarupaka <sagarmarupaka@gmail.com>
2022-04-15 10:02:43 +0530

    first commited

---
+++ src/main/java/in/sagar/GitPractiseApplication.java
@@ -0,0 +1,13 @@
+package in.sagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractiseApplication.class, args);
+	}
+
+}
